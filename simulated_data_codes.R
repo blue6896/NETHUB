853d870671a8fb76792bcabe7e1434aeed1d9e4b
blue6896 <f001c2p@dartmouth.edu>
2022-02-20 05:59:42 +0900

    Add files via upload

--- simulated_data_codes.R
+++ simulated_data_codes.R
@@ -1,5 +1,11 @@
 set.seed(12)
-simulated_data_codes <- function(nn,samples,case, numhub, bighub, shlb, shub,bigwgt,disconnected){
+simulated_data_codes <- function(nn,
+                                 samples,
+                                 case, 
+                                 numhub, 
+                                 bighub, 
+                                 bigwgt,
+                                 disconnected){
   
   ssigma <- matrix(0, nn,nn)
   diag(ssigma) <- 1
@@ -18,22 +24,11 @@ simulated_data_codes <- function(nn,samples,case, numhub, bighub, shlb, shub,big
     }
   }
   
-  ################################### tested so far
-  ################### modify the matrix
   counts<-0
-  
   yyy<-bighub
   conx <- matrix(0, nn, nn)
   for (i in 1:numhub) {
-    tmp<-order(dij[i,1:nn],decreasing=F)[1:bighub] # change here to make hubs or cliques
-    conx[i,tmp]<-1
-    conx[tmp,i]<-1
-  }
-  
-  #yyy<-smallhub
-  
-  for (i in (numhub+1):nn) {
-    tmp<-order(dij[i,1:nn],decreasing=F)[shlb:shub]
+    tmp<-order(dij[i,1:nn],decreasing=F)[1:bighub] # change here to make hubs
     conx[i,tmp]<-1
     conx[tmp,i]<-1
   }
@@ -63,12 +58,10 @@ simulated_data_codes <- function(nn,samples,case, numhub, bighub, shlb, shub,big
   premat1 <- sqrt(as.matrix(diag(premat))%*%t(as.matrix(diag(premat))))
   premat <- premat/premat1
   covmat<-solve(premat)
-  ##find sqaure root of a matrix
   eigencov<-eigen(covmat)
   D<-matrix(0,nn,nn)
   diag(D)<-sqrt(eigencov$values)
-  covmats<-eigencov$vectors %*%  D %*% t(eigencov$vectors)
-  #print("d")
+  covmats<-eigencov$vectors %*% D %*% t(eigencov$vectors)
   bighubnum<-1
   counts<- (counts+1)
   
@@ -101,26 +94,19 @@ simulated_data_codes <- function(nn,samples,case, numhub, bighub, shlb, shub,big
   else if (case ==3){
     #case3
     simudat[,(numhub+1):(numhub+numhub)] <- simudat[,(numhub+1):(numhub+numhub)] * bigwgt  # ((bigwgt-1) *3 +1)
-    #simudat[,1:floor(numhub/2)] <- simudat[,1:floor(numhub/2)] * bigwgt           # ((bigwgt-1) *3 +1)
-    #simudat[,(numhub+1):(numhub+floor((nn-numhub)/2))] <- simudat[,(numhub+1):(numhub+floor((nn-numhub)/2))] * bigwgt    # ((bigwgt-1) *3 +1)
   }
   else if (case ==4){  #random
     temp<-sample(nn,numhub)
-    simudat[,temp] <- simudat[,temp] * bigwgt      # ((bigwgt-1) *3 +1)
+    simudat[,temp] <- simudat[,temp] * bigwgt   
   }
   else if (case ==5){
-    simudat[,sample(nn,(nn/2))] <- simudat[,sample(nn,nn/2)] * bigwgt   #((bigwgt-1) *3 +1)
+    simudat[,sample(nn,(nn/2))] <- simudat[,sample(nn,nn/2)] * bigwgt
   }
   
   ##########
-  #print('jjj')
   s<-(simudat)
-  #print(class(s))
-  simufilename=paste("simudata-","nn",nn,"sp",samples,"nh",numhub,"bighub",bighub,"shlb",shlb,"shub",shub,"bigwgt",bigwgt,".txt", sep='')
-  #write.table(s,simufilename)
-  conxfilename=paste("originconx","nn",nn,"sp",samples,"nh",numhub,"bighub",bighub,"shlb",shlb,"shub",shub,"bigwgt",bigwgt,".txt", sep='')
-  #write.table(conx,conxfilename)
   newList <- list("simudata" = s, "originconx" = conx, "rawsimudata"=haha)
   write(conx,'origin.txt',ncol=nn)
   return(newList)
 }
+
